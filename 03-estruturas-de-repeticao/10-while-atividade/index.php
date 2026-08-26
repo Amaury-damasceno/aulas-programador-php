d6998f660f54315c52b5a3db584a8cabfe7b3178
Amaury Damasceno Soares <amaury.moto01@gmail.com>
2026-08-26 20:34:57 -0300

    resolução da atividade

--- 03-estruturas-de-repeticao/10-while-atividade/index.php
+++ 03-estruturas-de-repeticao/10-while-atividade/index.php
@@ -18,10 +18,24 @@ echo "<p>É a sua vez de ser atendido!</p>";
 
 echo "<h1>Atividade - 02</h1>";
 
-$limiteTotal = 20;
-$alunosNaSala = 0;
-$tentativas = 0;
-while ($limiteTotal >= 20) {
-echo "<p>Aluno numero: $limiteTotal excede a quantidade da sala.</p>";
-$limiteTotal--;
+const CAPACIDADE_MAXIMA = 20;
+
+$alunosNaSala = 1;
+$alunosEmAtraso = 0;
+
+while ($alunosNaSala <= CAPACIDADE_MAXIMA) {
+
+if ($alunosNaSala % 3 === 0){
+echo "<P>Bloqueado: Aluno {$alunosNaSala} está coma mensalidade em atraso </p>";
+$alunosEmAtraso++; // quantidade de alunos em atraso.
+$alunosNaSala++;
+continue;
 }
+
+
+echo "<p>Aluno liberado. Entrada {$alunosNaSala} de " . CAPACIDADE_MAXIMA . ".</p>";
+$alunosNaSala++;
+}
+
+echo"<p><strong> Capacidade Maxima atingida</strong></p>";
+echo"<p>Total de bloqueios realizados: {$alunosEmAtraso}</p>";
