64ccf807c4e8e8911d09e80cc3bb214f8c9b0886
Amaury Damasceno Soares <amaury.moto01@gmail.com>
2026-09-09 21:45:36 -0300

    feat: adicionar a pasta 06 - formulario e uma pratica em GET

---
+++ 06-formularios-parte1/18-formularios-get-pratica/busca.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario - Get</title>
+</head>
+<body>
+    
+<form action="index.php" method="get">
+    <label for="livro">Procure pelo titulo do livro</label>
+    <br>
+    <input type="text" name="livro" id="livro">
+
+    <button type="submit">Busca</button>
+
+</body>
+</html>
---
+++ 06-formularios-parte1/18-formularios-get-pratica/index.php
@@ -0,0 +1,9 @@
+<?php
+require __DIR__ . "/../../senac/senac.php";
+senacClassName("Formularios - get - prática");
+
+echo "<h1>Atividade:</h1>";
+
+var_dump(
+    $_GET
+);
